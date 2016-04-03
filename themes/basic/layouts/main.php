47383c2c7ed81d42609cb2f500b971078b36b859
lulubin <452936616@qq.com>
2016-04-03 15:08:32 +0800

    20160403修改

--- themes/basic/layouts/main.php
+++ themes/basic/layouts/main.php
@@ -5,7 +5,6 @@ use yii\bootstrap\NavBar;
 use yii\bootstrap\Alert;
 use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
-use yii\helpers\Url;
 use app\modules\admin\models\config\Config;
 
 AppAsset::register($this);
@@ -87,30 +86,7 @@ AppAsset::register($this);
         <div><?= $content;?></div>
     </div>
 </div>
-
-<footer class="footer">
-    <div class="container">
-        <div class='row'>
-            <div class='col-lg-3'>
-                <dt>网站信息</dt>
-                <dd><a href="<?= Url::to(['/site/about'])?>">关于我们</a></dd>
-            </div>
-            <div class='col-lg-3'>
-                <dt>相关合作</dt>
-                <dd><a href="<?= Url::to(['/site/contact'])?>">联系我们</a></dd>
-            </div>
-            <div class='col-lg-3'>
-                <dt>关注我们</dt>
-                <dd><a href="<?= Url::to(['/site/diary'])?>">成长日记</a></dd>
-            </div>
-            <div class='col-lg-3'>
-                <dt>技术采用</dt>
-                <dd><?= Yii::powered() ?> <?= Yii::getVersion() ?></dd>
-            </div>
-        </div>
-    </div>
-</footer>
-
+<?=$this->render('footer')?>
 <?php $this->endBody() ?>
 </body>
 </html>
