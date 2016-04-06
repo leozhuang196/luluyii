d9b2d17ddbc06dafa38773e971ad54d75a4f4a4c
mahaobin <452936616@qq.com>
2016-04-06 08:49:35 +0800

    201160406删除'sourceLanguage' => 'zh-CH'

---
+++ modules/user/messages/zh-CN/user.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'Username' => '用户名',
+    'Password' => '密码',
+    'RePassword' => '重复密码',
+    'Remember Me' => '记住我',
+    'Email' => '邮箱',
+    'Old Password' => '旧密码',
+    'New Password' => '新密码',
+    'Renew Password' => '重复新密码',
+    'VerifyCode'=> '验证码',
+];
