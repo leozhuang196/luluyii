9dc1b3ab32011f422c08a9f6ab22598389a443de
lulubin <452936616@qq.com>
2016-05-12 14:31:13 +0800

    首页还是图片就好

--- themes/basic/site/index.php
+++ themes/basic/site/index.php
@@ -1,6 +1,7 @@
 <?php
+use yii\helpers\Html;
 $this->title = \Yii::$app->params['siteName'];
 ?>
 <div align="center">
-<video width="600" height="400" src="videos/football.mp4" controls="controls"></video>
+<?= Html::img('images/01.jpg')?>
 </div>
