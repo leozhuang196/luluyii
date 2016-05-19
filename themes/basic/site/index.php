bd68568680c20297333e4b50931756901f321397
lulubin <452936616@qq.com>
2016-05-19 23:17:39 +0800

    修复首页链接

--- themes/basic/site/index.php
+++ themes/basic/site/index.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 use yii\helpers\Html;
 use modules\user\models\SigninForm;
 $this->title = \Yii::$app->params['siteName'];
@@ -11,11 +11,11 @@ $date = date('Y年m月d日',time());
     <div class='col-md-3'>
     	<div class="btn-group btn-group-justified">
     	<?php if (SigninForm::isNotSignin()):?>
-    		<?= Html::a('<i class="fa fa-calendar-plus-o"></i> 点此处签到<br>签到有好礼','/user/default/signin',['class'=>"btn btn-success"])?>
+    		<?= Html::a('<i class="fa fa-calendar-plus-o"></i> 点此处签到<br>签到有好礼',['/user/default/signin'],['class'=>"btn btn-success"])?>
 		<?php else :?>
 			<?= Html::a('<i class="fa fa fa-calendar-check-o"></i>  今日已签到<br>已连续'.SigninForm::siginDay().'天','javascript:void(0)',['class'=>"btn btn-success disabled"])?>
 		<?php endif;?>
-    		<?= Html::a($date.'<br>已有'.SigninForm::siginNum().'人签到</a>','/user/default/signin-member',['class'=>"btn btn-primary"])?>
+    		<?= Html::a($date.'<br>已有'.SigninForm::siginNum().'人签到</a>',['/user/default/signin-member'],['class'=>"btn btn-primary"])?>
         </div>
     </div>
 </div>
