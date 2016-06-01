597cce993474fb0a5b392f99d5ed4a16ab70bd16
lulubin <452936616@qq.com>
2016-06-01 09:00:45 +0800

    网站日访问量bug修复

--- models/Visit.php
+++ models/Visit.php
@@ -14,16 +14,18 @@ class Visit extends \yii\db\ActiveRecord
     {
         $visit_ip = Yii::$app->request->userIP;
         $visit = Visit::findOne(['visit_ip' => $visit_ip]);
-        $yesterdayZeroTime = mktime(0,0,0,date('m')-1,date('d'),date('Y'));
-        if ($visit && $visit->visit_time>$yesterdayZeroTime){
-            $visit->visit_time = time();
-            $visit->save();
+        $todayZeroTime = mktime(0,0,0,date('m'),date('d'),date('Y'));
+        if ($visit){
+            if ($visit->visit_time<$todayZeroTime){
+                $visit->visit_time = time();
+                $visit->save();
+            }
         }else{
             $visit = new Visit();
             $visit->visit_ip = $visit_ip;
             $visit->visit_time = time();
             $visit->save();
         }
-        return Visit::find()->where(['>','visit_time',$yesterdayZeroTime])->count();
+        return Visit::find()->where(['>','visit_time',$todayZeroTime])->count();
     }
 }
