252e80b03cfc216dea2b1de2fe52b05165cbca84
lulubin <452936616@qq.com>
2016-06-06 16:02:09 +0800

    数据库连接配置

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=luluyii',
     'username' => 'root',
-    'password' => 'mhba82264322',
+    'password' => 'root',
     'charset' => 'utf8',
     'tablePrefix' => 'lulu_',
 ];
