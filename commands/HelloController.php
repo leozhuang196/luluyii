6d9505c45466fc382e75dd095e93fb2f28ec2cd0
lulubin <452936616@qq.com>
2016-06-06 15:50:58 +0800

    yii命令行controller

--- commands/HelloController.php
+++ commands/HelloController.php
@@ -19,10 +19,6 @@ use yii\console\Controller;
  */
 class HelloController extends Controller
 {
-    /**
-     * This command echoes what you have entered as the message.
-     * @param string $message the message to be echoed.
-     */
     public function actionIndex($message = 'hello world')
     {
         echo $message . "\n";
